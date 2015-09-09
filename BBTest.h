46223cb0d6d85798dc4f24389a0bdda152aa55c0
zgc <git@zgc.com>
2015-09-09 10:16:28 +0800

    add podspec ,BBTest class

---
+++ BBTest.h
@@ -0,0 +1,13 @@
+//
+//  BBTest.h
+//  BBFoundation
+//
+//  Created by apple on 15/9/8.
+//  Copyright (c) 2015年 app. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BBTest : NSObject
+- (void)test;
+@end
---
+++ BBTest.m
@@ -0,0 +1,16 @@
+//
+//  BBTest.m
+//  BBFoundation
+//
+//  Created by apple on 15/9/8.
+//  Copyright (c) 2015年 app. All rights reserved.
+//
+
+#import "BBTest.h"
+
+@implementation BBTest
+- (void)test
+{
+    NSLog(@"BBTest is from pods");
+}
+@end
